865af85403b9e3d63ccf94aa97f2e715a1b9ce05
luoxinran28 <luoxinran28@gmail.com>
2019-09-11 22:20:08 -0600

    Minimum Path Sum

---
+++ JS/64.minimum-path-sum.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=64 lang=javascript
+ *
+ * [64] Minimum Path Sum
+ */
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var minPathSum = function(grid) {
+  if (grid.length === 0 || grid[0].length === 0) return 0;
+  let rows = grid.length;
+  let columns = grid[0].length;
+  let dp = Array.from({ length: rows }).map(() =>
+    Array.from({ length: columns }).fill(0)
+  );
+  dp[0][0] = grid[0][0];
+  for (let i = 1; i < rows; i++) {
+    dp[i][0] = dp[i - 1][0] + grid[i][0];
+  }
+  for (let j = 1; j < columns; j++) {
+    dp[0][j] = dp[0][j - 1] + grid[0][j];
+  }
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < columns; j++) {
+      dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i][j];
+    }
+  }
+  return dp[rows - 1][columns - 1];
+};
