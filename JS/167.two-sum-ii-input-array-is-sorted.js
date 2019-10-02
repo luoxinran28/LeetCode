f3dfba39d7fef3baf998bc83cc7bf317d3bc99ee
luoxinran28 <luoxinran28@gmail.com>
2019-10-01 20:48:34 -0600

    Two sum ii input array is sorted

---
+++ JS/167.two-sum-ii-input-array-is-sorted.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=167 lang=javascript
+ *
+ * [167] Two Sum II - Input array is sorted
+ */
+/**
+ * @param {number[]} numbers
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(numbers, target) {
+  // Two pointers
+  let left = 0,
+    right = numbers.length - 1;
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+    if (sum === target) {
+      return [left + 1, right + 1];
+    } else if (sum < target) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return [];
+};
