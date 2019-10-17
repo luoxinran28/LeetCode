eebe8f3353783845b3a0c34e4183188610697009
luoxinran28 <luoxinran28@gmail.com>
2019-10-16 22:23:05 -0600

    Climbing stairs

---
+++ JS/70.climbing-stairs.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=70 lang=javascript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+  if (n == 0) return 1;
+  if (n <= 2) return n;
+  let dp = Array.from({ length: n + 1 }).fill(0);
+  dp[0] = 0;
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+};
+// @lc code=end
