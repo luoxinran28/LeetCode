bec7d4d4334a7133c8340af919eafca11f2944e4
luoxinran28 <luoxinran28@gmail.com>
2019-11-16 09:28:27 -0700

    Rectangle overlap - lintcode

--- JS/836.rectangle-overlap.js
+++ JS/836.rectangle-overlap.js
@@ -12,7 +12,20 @@
  */
 // https://leetcode.com/problems/rectangle-overlap/discuss/132340/C%2B%2BJavaPython-1-line-Solution-1D-to-2D
 var isRectangleOverlap = function(rec1, rec2) {
-  return rec1[0] < rec2[2] && rec2[0] < rec1[2] && rec1[1] < rec2[3] && rec2[1] < rec1[3];
+  return (
+    rec1[0] < rec2[2] &&
+    rec2[0] < rec1[2] &&
+    rec1[1] < rec2[3] &&
+    rec2[1] < rec1[3]
+  );
 };
-// @lc code=end
 
+// const doOverlap = function (l1, r1, l2, r2) {
+//     // write your code here
+//   // Four scenarios: https://www.youtube.com/watch?v=wx0nyomRS_U
+//   if(l2.x > r1.x || l1.x > r2.x) return false;
+//   if(r2.y > l1.y || r1.y > l2.y) return false;
+//   return true;
+// }
+
+// @lc code=end
