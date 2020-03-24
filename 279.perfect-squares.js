fbb2df0079de02418ade5cf1fc9ef0ebb8d8ac43
luoxinran28 <luoxinran28@gmail.com>
2020-03-23 22:57:10 -0600

    Perfect square

---
+++ 279.perfect-squares.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode id=279 lang=javascript
+ *
+ * [279] Perfect Squares
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var numSquares = function(n) {
+  let dp = Array(n + 1).fill(n);
+  dp[0] = 0;
+  dp[1] = 1;
+  for(let i = 1; i <= n; i++) {
+    for(let j = 1; j * j <= i; j++) {
+      dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+    }
+  }
+  return dp[n];
+};
+// @lc code=end
+
